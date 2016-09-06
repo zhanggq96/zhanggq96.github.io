d52b417aa2d5fc072bfd443ddab6065c8f52c33e
George Zhang <zhanggq@mcmaster.ca>
2016-09-06 00:59:34 -0400

    Improved Gallery

--- js/show-hide.js
+++ js/show-hide.js
@@ -1,9 +1,19 @@
+// jQuery(function(){
+//          jQuery('#showall').click(function(){
+//                jQuery('.description').show();
+//         });
+//         jQuery('.container').click(function(){
+//               jQuery('.description').hide();
+//               jQuery('#item'+$(this).attr('target')).show();
+//         });
+// });
+
 jQuery(function(){
          jQuery('#showall').click(function(){
                jQuery('.description').show();
         });
-        jQuery('.container').click(function(){
-              jQuery('.description').hide();
+        jQuery('div[type]').click(function(){
+              jQuery('.description-'+$(this).attr('type')).hide();
               jQuery('#item'+$(this).attr('target')).show();
         });
 });
